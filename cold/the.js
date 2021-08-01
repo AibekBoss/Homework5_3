643e0eb16c83acf3c7a5a9116ed08aca2a61a179
Пользователь <AibekBoss>
2021-08-01 13:31:20 +0600

    hello

--- cold/the.js
+++ cold/the.js
@@ -11,25 +11,30 @@
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
- let att = [
+let att = [
     { name: 'rouse', price: 300 },
     { name: 'sunflower', price: 1000 },
     {name: '101 rouse', price: 3000 }
 ]
 
 
-module.exports.call =  att.filter((the)=>{
+
+
+let call = att.filter((the)=>{
+    let withDiscount = {}
      if (the.price>=200&&the.price<900){
-         return the.price=(the.price-(5/100*the.price))
+         return the.withDiscount=(the.price-(5/100*the.price))
      }
      if(the.price>=900&&the.price<2500){
-         return the.price=(the.price-(10/100*the.price))
+         return the.withDiscount=(the.price-(10/100*the.price))
      }
      if(the.price>=2500){
-         return the.price=(the.price-(15/100*the.price))
+         return the.withDiscount=(the.price-(15/100*the.price))
      }
 
 })
+
+
 
 
 
